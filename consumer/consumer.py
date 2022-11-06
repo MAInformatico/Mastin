89fa13c23edbb77c3a7b079ffac6c4cd5d02cae7
Mike <magutierrezsistemas@gmail.com>
2022-11-06 18:36:30 +0100

    consumer ready

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,10 +1,10 @@
 from plyer import notification
 import pika
 
-server = '192.168.1.39'
-currentPort = '5672'
+server = 'IP server'
+currentPort = 'your port'
 #declaring the credentials needed for connection like host, port, username, password, exchange etc
-credentials = pika.PlainCredentials('user','user')
+credentials = pika.PlainCredentials('user','userPassword')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=server, port=currentPort, credentials= credentials))
 channel = connection.channel()
 channel.exchange_declare('test', durable=True, exchange_type='topic')#defining callback functions responding to corresponding queue callbacks
