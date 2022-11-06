e37f9fc499456f3881235c52846ca5afd140b01b
Mike <magutierrezsistemas@gmail.com>
2022-11-06 18:32:23 +0100

    consumer ready

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -9,10 +9,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=server, port
 channel = connection.channel()
 channel.exchange_declare('test', durable=True, exchange_type='topic')#defining callback functions responding to corresponding queue callbacks
 def callbackFunctionForQueue(ch,method,properties,body):
- print('Message received from server: ', body)
+ #print('Message received from server: ', body)
+ notification.notify(
+    title='¡Warning!',
+    message = body)
+
 channel.basic_consume(queue='comunicationQueue', on_message_callback=callbackFunctionForQueue, auto_ack=True)
 #this will be command for starting the consumer session
 channel.start_consuming()
-notification.notify(
-    title='Fin de simulación',
-    message='La simulación ha finalizado')
