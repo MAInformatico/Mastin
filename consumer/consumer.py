631e76ef29cc3ad4bf0cee635ceeb9a83b618b77
Mike <magutierrezsistemas@gmail.com>
2022-11-07 09:55:37 +0100

    adding notifications for various OS

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,6 +1,9 @@
 from plyer import notification
 import pika
 
+import os
+import platform
+
 server = 'IP server'
 currentPort = 'your port'
 #declaring the credentials needed for connection like host, port, username, password, exchange etc
@@ -10,9 +13,16 @@ channel = connection.channel()
 channel.exchange_declare('test', durable=True, exchange_type='topic')#defining callback functions responding to corresponding queue callbacks
 def callbackFunctionForQueue(ch,method,properties,body):
  #print('Message received from server: ', body)
- notification.notify(
-    title='Warning!',
-    message = body)
+    lTitle = 'Warning'
+    lMessage = body
+    if platform.system() == 'Darwin':
+        os.system("osascript -e 'display notification \"{}\" with title \"{}\"'".format(lMessage, lTitle))
+    else:
+        notification.notify(
+            title=lTitle,
+            message = lMessage,
+            app_icon='python.ico')
+        
 
 channel.basic_consume(queue='communicationQueue', on_message_callback=callbackFunctionForQueue, auto_ack=True)
 #this will be command for starting the consumer session
